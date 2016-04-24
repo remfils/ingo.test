04d4d4783a9a8a1dfc8bd780fa1215efa294d349
Vlad Pereskokov <pereskokow@gmail.com>
2016-04-24 12:49:37 +0300

    sceleton of api for js code

--- app/controllers.php
+++ app/controllers.php
@@ -1,3 +1,11 @@
 <?php
 
 $app->get('/', 'App\\Controllers\\MainController::indexAction');
+
+/* API */
+
+$app->get('/api/all-movies', 'App\\Controllers\\ApiController::allMoviesAction');
+
+$app->get('/api/movie/small/{id}', 'App\\Controllers\\ApiController::smallMovieDescription');
+
+$app->get('/api/movie/full/{id}', 'App\\Controllers\\ApiController::largeMovieDescription');
