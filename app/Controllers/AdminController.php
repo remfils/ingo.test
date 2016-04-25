b52d58dbb608f6f131a3cafdb1a317887842037c
Vlad Pereskokov <pereskokow@gmail.com>
2016-04-25 12:57:43 +0300

    adds simple edit project table

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -21,7 +21,7 @@ class AdminController
 
         return 'NOPE';
     }
-
+/*D!*/
     public function loginAction( Request $req, Application $app )
     {
         $default = array(
@@ -57,5 +57,35 @@ class AdminController
             'login_form' => $login_form->createView(),
             'pass' => $pass
         ));
+    }
+
+    public function editProjectAction( Request $req, Application $app ) {
+        $id = $req->attributes->get('id');
+
+        $q = $app['db']->prepare('select * from projects p join project_description pd on pd.movie_id = p.id where p.id = :id');
+        $q->bindValue(':id', $id);
+        $q->execute();
+        $movie = $q->fetch();
+
+        $q = $app['db']->prepare('select * from project_fields p where p.movie_id = :id');
+        $q->bindValue(':id', $id);
+        $q->execute();
+        $table = $q->fetchAll();
+
+        $form = $app['form.factory']->createBuilder('form', $movie)
+            ->add('name', 'text')
+            ->add('color', 'text')
+            ->add('year', 'text')
+            ->add('logo', 'text')
+            ->add('preview_url', 'text')
+            ->add('description', 'textarea', array(
+                'attr' => array('rows' => '10')
+            ))
+            ->add('submit', 'submit', array(
+                'attr' => array('class' => 'btn-submit')
+            ))
+            ->getForm();
+
+        return $app['twig']->render('admin/edit-project.html.twig', array('form'=>$form->createView()));
     }
 }
--- app/controllers.php
+++ app/controllers.php
@@ -8,8 +8,9 @@ $app->get('/api/all-movies', 'App\\Controllers\\ApiController::allMoviesAction')
 
 $app->get('/api/movie/{id}', 'App\\Controllers\\ApiController::movieDescriptionAction');
 
+/*D!*/
 $app->get('/api/movie/small/{id}', 'App\\Controllers\\ApiController::smallMovieDescription');
-
+/*D!*/
 $app->get('/api/movie/full/{id}', 'App\\Controllers\\ApiController::largeMovieDescription');
 
 /* ADMIN */
@@ -19,3 +20,5 @@ $app->match('/login', 'App\\Controllers\\AdminController::loginAction', '(GET|PO
 
 $app->get('/admin', 'App\\Controllers\\AdminController::indexAction')
     ->bind('admin');
+
+$app->match('/admin/edit/project/{id}', 'App\\Controllers\\AdminController::editProjectAction', '(GET|POST)');
