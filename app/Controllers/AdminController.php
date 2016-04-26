3d82433eb47f575296777cf71f4c298b50495448
Pereskokov V <pereskokow@gmail.com>
2016-04-26 13:17:21 +0300

    rewrite add-project form

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class AdminController
 {
@@ -60,40 +62,57 @@ class AdminController
     }
 
     public function addProjectAction( Request $req, Application $app ) {
-        $form = $app['form.factory']->createBuilder('form', $movie)
-            ->add('name', 'text')
-            ->add('year', 'text')
-            ->add('logo', 'file')
-            ->add('preview_url', 'text')
-            ->add('color', 'text')
-            ->add('description', 'textarea', array(
-                'attr' => array('rows' => '10')
-            ))
-            ->add('submit', 'submit', array(
-                'attr' => array('class' => 'btn-submit')
-            ))
-            ->getForm();
-
-        $form->handleRequest($req);
-
         $success_message = '';
 
-        if ( $form->isValid() ) {
-            $data = $form->getData();
+        if ( $req->isMethod('POST') ) {
+            $data = array();
+            $data['name'] = $req->get('name');
+            $data['color'] = $req->get('color');
+            $data['year'] = $req->get('year');
+            $data['logo'] = $req->get('logo');
+            $data['preview_url'] = $req->get('preview_url');
+            $data['description'] = $req->get('description');
 
-            $q = $app['db']->prepare('update projects set name = :name, color = :color, year=:year, logo=:logo where movie_id = :movie_id');
+            $q = $app['db']->prepare('INSERT INTO projects (name, color, year, logo) VALUES (:name, :color, :year, :logo)');
             $q->bindValue(':name', $data['name']);
             $q->bindValue(':color', $data['color']);
             $q->bindValue(':year', $data['year']);
             $q->bindValue(':logo', $data['logo']);
+            $q->execute();
+
+            $id = $app['db']->lastInsertId();
+
+            $q = $app['db']->prepare('INSERT INTO project_description (preview_url, description, movie_id) VALUES (:preview_url, :description, :movie_id)');
+            $q->bindValue(':preview_url', $data['preview_url']);
+            $q->bindValue(':description', $data['description']);
             $q->bindValue(':movie_id', $id);
             $q->execute();
 
-            $success_message = 'project successfully edited!';
+            $fields = array();
+
+            foreach ( $req->request->all() as $key => $value ) {
+                if ( preg_match('/^field_(?P<name>.+)_(?P<id>.+)/', $key, $matches ) ) {
+                    if ( isset($fields[$matches['id']]) ) {
+                        $fields[$matches['id']][$matches['name']] = $value;
+                    }
+                    else {
+                        $fields[$matches['id']] = array( $matches['name'] => $value);
+                    }
+                }
+            }
+
+            foreach ( $fields as $key => $field ) {
+                $q = $app['db']->prepare('INSERT INTO project_fields (movie_id, field_name, field_value) VALUES (:movie_id, :field_name, :field_value)');
+                $q->bindValue(':field_name', $field['name']);
+                $q->bindValue(':field_value', $field['value']);
+                $q->bindValue(':movie_id', $id);
+                $q->execute();
+            }
+
+            $success_message = 'project successfully created! ';
         }
 
         return $app['twig']->render('admin/new-project.html.twig', array(
-            'form'=>$form->createView(),
             'success_message' => $success_message
         ));
     }
