99e4bcab256cf7adb669f6757e44011515901bce
Vlad Pereskokov <pereskokow@gmail.com>
2016-06-13 20:27:52 +0300

    config changes

--- web/js/app/config.js
+++ web/js/app/config.js
@@ -1,13 +1,13 @@
 var config = {
     DEBUG: false,
-    HOME: false,
+    HOME: true,
     SITE_NAME: "http://ingo-test.tk/"
 };
 
 if ( config.HOME ) {
-     config.SITE_NAME = "http://ingo.test/web/";
+     config.SITE_NAME = "http://ingo.test/";
 };
 
-config['ASSET_PATH'] = config.SITE_NAME;
+config['ASSET_PATH'] = config.SITE_NAME + "web/";
 
 export default config;
