2a877d9d0e2607ec15186fab455a817a32680077
Vlad Pereskokov <pereskokow@gmail.com>
2016-06-29 01:50:04 +0300

    IndexPage: new design for title-table animation transition

--- web/js/app/pages/IndexPage/TitleColoredTable.js
+++ web/js/app/pages/IndexPage/TitleColoredTable.js
@@ -15,8 +15,12 @@ export default class TitleColoredTable extends React.Component {
         this.id = "TitleColoredTable" + TitleColoredTable.box_counter;
 
         this.state = {
-            is_transition: false
+            is_transition: false,
+            current_color: ""
         };
+
+        this.current_color = "";
+        this.next_color = "";
     }
 
     get id() {
@@ -28,17 +32,42 @@ export default class TitleColoredTable extends React.Component {
     }
 
     componentWillUpdate( nextProps, nextState ) {
-        var current_color = this.props.color;
         var next_color = nextProps.color;
+        if ( !this.current_color ) {
+            this.current_color = nextProps.color;
+        }
+        else if ( this.current_color != next_color ) {
+            this.next_color = next_color;
 
-        TweenLite.to($(this.id), 2, {backgroundColor: next_color});
+            $("#TableForegroundColor")
+                .css("width", "100%")
+                .removeClass("left")
+                .addClass("right");
+
+            TweenLite.to($("#TableForegroundColor"), 1, {width: 0, onComplete: () => {
+                this.current_color = next_color;
+                this.setState(current_color, next_color);
+                this.next_color = "";
+            }});
+        }
     }
 
     render() {
         var table_style = { background: this.color };
 
-        return <table id={this._id} className={this.props.className} style={ table_style }>
-            { this.props.children }
-        </table>;
+        var current_bg_color = { backgroundColor: this.current_color };
+        var next_bg_color = {};
+
+        if ( this.next_color ) {
+            next_bg_color = { backgroundColor: this.next_color };
+        }
+
+        return <div className={this.props.className}>
+            <div id="TableBackgroundColor" style={next_bg_color}></div>
+            <div id="TableForegroundColor" style={current_bg_color}></div>
+            <table id={this._id} >
+                { this.props.children }
+            </table>
+        </div>;
     }
 }
