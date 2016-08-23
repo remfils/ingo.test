e226daa6115f16163f3d5288eddefb68f16d2fa7
Pereskokov Vlad @CFU <pereskokow@gmail.com>
2016-08-23 16:17:14 +0300

    added menu right of navigation

--- web/js/app/pages/components/NavigationMenu.js
+++ web/js/app/pages/components/NavigationMenu.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-import { createNotReadyYetFunction } from "../../funcitons";
+import { asset, notReadyYet, createNotReadyYetFunction } from "../../funcitons";
 import * as TransitionActions from "../../actions/TransitionActions";
 
 var $ = require('jquery');
 
 export default class NavigationMenu extends React.Component {
+
+    static PAGE_INDEX = 'INDEX';
+    static PAGE_WORK = 'WORK';
+    static PAGE_ABOUT = 'ABOUT';
+    static PAGE_CONTACTS = 'CONTACTS';
 
     newsClickListener(e) {
         e.preventDefault();
@@ -13,6 +18,21 @@ export default class NavigationMenu extends React.Component {
         TransitionActions.fromContactToIndexTransition(this.props.current_page);
 
         return false;
+    }
+
+    createClickFunctionForPage(page_name) {
+        var self = this;
+
+        return function(e) {
+            e.preventDefault();
+
+            if (page_name == self.props.page_name)
+                return false;
+
+            TransitionActions.createTitleTransition(self.props.page_name, page_name, self.props.current_page);
+
+            return false;
+        }
     }
 
     contactClickListener(e) {
@@ -23,12 +43,23 @@ export default class NavigationMenu extends React.Component {
         return false;
     }
 
+    titleMenuClickListener(e) {
+        e.preventDefault();
+
+        notReadyYet('Menu');
+
+        return false;
+    }
+
     render() {
-        return <ul>
-            <li><a href="#" onClick={this.newsClickListener.bind(this)}>news</a></li>
-            <li><a href="#" onClick={createNotReadyYetFunction("work")}>work</a></li>
-            <li><a href="#" onClick={createNotReadyYetFunction("about")}>about</a></li>
-            <li><a href="#" onClick={this.contactClickListener.bind(this)}>contacts</a></li>
-        </ul>;
+        return <div class='title-menu'>
+                <ul>
+                    <li><a href="#" class={this.props.page_name == NavigationMenu.PAGE_INDEX ? 'active' : ''} onClick={this.createClickFunctionForPage(NavigationMenu.PAGE_INDEX)}>news</a></li>
+                    <li><a href="#" class={this.props.page_name == NavigationMenu.PAGE_WORK ? 'active' : ''} onClick={createNotReadyYetFunction("work")}>work</a></li>
+                    <li><a href="#" class={this.props.page_name == NavigationMenu.PAGE_ABOUT ? 'active' : ''} onClick={createNotReadyYetFunction("about")}>about</a></li>
+                    <li><a href="#" class={this.props.page_name == NavigationMenu.PAGE_CONTACTS ? 'active' : ''} onClick={this.createClickFunctionForPage(NavigationMenu.PAGE_CONTACTS)}>contacts</a></li>
+                </ul>
+                <span class='btn-menu' onClick={this.titleMenuClickListener.bind(this)}><img src={asset('img/button-menu.png')} alt=""/></span>
+            </div>;
     }
 }
