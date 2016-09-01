09958bb04f2f002bd5e39298de61235078960862
Pereskokov Vlad @CFU <pereskokow@gmail.com>
2016-09-01 11:27:56 +0300

    Kontactformular send is working in ajax

--- web/js/app/pages/MoviePage/EmailForm.js
+++ web/js/app/pages/MoviePage/EmailForm.js
@@ -1,16 +1,28 @@
 import React from "react";
 
 import {notReadyYet} from '../../funcitons.js';
+import config from '../../config';
 
 var $ = require('jquery');
 
+const POST_URL = 'url';
+
 export default class EmailForm extends React.Component {
     static box_counter = 0;
+
+    static STATE_INIT = 'FORM_STATE_INIT';
+    static STATE_MESSAGE_IS_SENDING = 'FORM_STATE_MESSAGE_IS_SENDING';
+    static STATE_MESSAGE_SEND_FAILED = 'FORM_STATE_MESSAGE_SEND_FAILED';
+    static STATE_MESSAGE_IS_SENT = 'FORM_STATE_MESSAGE_IS_SENT';
 
     constructor() {
         super();
 
         EmailForm.box_counter ++;
+
+        this.state = {
+            form_state: EmailForm.STATE_INIT
+        };
 
         this.id = "EmailForm" + EmailForm.box_counter;
     }
@@ -26,14 +38,58 @@ export default class EmailForm extends React.Component {
     sendenClickListener(e) {
         e.preventDefault();
 
-        notReadyYet("Sended Click");
+        this.setState({
+            form_state: EmailForm.STATE_MESSAGE_IS_SENDING
+        });
+
+        var post_data = {};
+
+        $('.email-form :input').each((i, item)=>{
+            post_data[$(item).attr('name')] = $(item).val();
+        });
+
+        console.debug('sendenClickListener: sending ', post_data);
+
+        $.ajax({
+            url: config.SITE_NAME + POST_URL,
+            dataType: 'json',
+            data: post_data,
+            success: (response) => {
+                if (response.messageIsSent) {
+                    this.setState({form_state: EmailForm.STATE_MESSAGE_IS_SENT});
+                }
+            },
+            error: (err) => {
+                this.setState({form_state: EmailForm.STATE_MESSAGE_SEND_FAILED});
+                console.log('ERROR:  ' + err);
+            }
+        });
 
         return false;
     }
 
     render() {
+        var senden_text = 'SENDEN';
+        var response_msg = '';
+
+        console.debug(this.state.form_state);
+
+        if (this.state.form_state === EmailForm.STATE_MESSAGE_IS_SENDING) {
+            senden_text = 'SENDEN...';
+        }
+
+        if (this.state.form_state === EmailForm.STATE_MESSAGE_SEND_FAILED) {
+            response_msg = <h2>sending message failed</h2>;
+        }
+
+        if (this.state.form_state === EmailForm.STATE_MESSAGE_IS_SENT) {
+            response_msg = <h2>message is sent</h2>;
+        }
+
         return <form id={this._id} className={"email-form " + this.props.className}>
             <h1>KONTAKTFORMULAR</h1>
+
+            {response_msg}
 
             <div class="custom-form-gruop">
                 <label for="name">Name*</label>
@@ -51,7 +107,7 @@ export default class EmailForm extends React.Component {
             </div>
 
             <div class="custom-form-gruop">
-                <input type="submit" value="SENDEN" onClick={this.sendenClickListener.bind(this)}/>
+                <input type="submit" value={senden_text} onClick={this.sendenClickListener.bind(this)}/>
             </div>
 
             <p>++ Ingo Scheel Kameramann I DOP für: Imagefilm I Werbung I Spielfilm I Dokumentarfilm I Köln ++</p>
