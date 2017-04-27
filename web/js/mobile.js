21ee2ec7999d2fc3535d22aadecd64afd1d500c1
Vlad@CFU <pereskokow@gmail.com>
2017-04-27 14:56:12 +0300

    added mobile js

---
+++ web/js/mobile.js
@@ -0,0 +1,12 @@
+(function($) {
+  $('body').on( "swiperight", swiperightHandler );
+  function swiperightHandler(event){
+    
+  }
+
+  //funtion смах пальца 
+  $('body').on( "swipeleft", swipeleftHandler );
+  function swipeleftHandler(event){
+    
+  }
+})($);
