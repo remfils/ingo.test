66c9b0a8484e991b5c991c28513e7e64d3c01a65
Vlad@home <pereskokow@gmail.com>
2017-05-08 13:27:14 +0300

    added doc.org and dirlocals

---
+++ web/js/app/.dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js-mode
+  (js-indent-level . 4))
+ (js2-mode
+  (js-indent-level . 4))
+ (web-mode
+  (js-indent-level . 4)))
+
+
