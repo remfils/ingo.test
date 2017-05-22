f0dcaed61eb23d44e0e5b2556af61781d5e6c683
Vlad@home <pereskokow@gmail.com>
2017-05-22 22:55:49 +0300

    added config

--- app/config.php
+++ app/config.php
@@ -4,11 +4,12 @@ $app['debug'] = true;
 
 /* assets */
 $app['sub_domain'] = '';
+//$app['sub_domain'] = 'ingo-test';
 $app['asset_path'] = 'http://'. $_SERVER['SERVER_NAME'] . $app['sub_domain'] .'/web/';
 
-/*$db_name = "ingo";
+$db_name = "ingo";
 $db_user = "ingo";
-$db_pass = "777qwe777";*/
+$db_pass = "777qwe777";
 
 $db_name = "wsh1490_ingo";
 $db_user = "wsh1490_ingo";
